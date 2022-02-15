05e6390eeacf1e2a2ac797d5a3b5e8f5f6b6bc19
Ed Page <eopage@gmail.com>
2022-02-15 12:17:39 -0600

    docs: Update test style

--- src/lib.rs
+++ src/lib.rs
@@ -109,19 +109,17 @@
 /// ```should_panic
 /// use assert_cmd::Command;
 ///
-/// fn main() {
-///     let mut cmd = Command::cargo_bin(assert_cmd::crate_name!()).unwrap();
-///     let assert = cmd
-///         .arg("-A")
-///         .env("stdout", "hello")
-///         .env("exit", "42")
-///         .write_stdin("42")
-///         .assert();
-///     assert
-///         .failure()
-///         .code(42)
-///         .stdout("hello\n");
-/// }
+/// let mut cmd = Command::cargo_bin(assert_cmd::crate_name!()).unwrap();
+/// let assert = cmd
+///     .arg("-A")
+///     .env("stdout", "hello")
+///     .env("exit", "42")
+///     .write_stdin("42")
+///     .assert();
+/// assert
+///     .failure()
+///     .code(42)
+///     .stdout("hello\n");
 /// ```
 #[macro_export]
 macro_rules! crate_name {
